6ca4473d204dbfcb7a2f33999db9d6630e014fca
kartikaysharma01 <sharma.kartik2107@gmail.com>
2020-09-21 21:58:45 +0530

    Initial commit
    Login Layout

---
+++ app/src/main/java/com/example/fuelistic_test/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.fuelistic_test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static int SPLASH_SCREEN= 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i =new Intent(MainActivity.this, Login.class);
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_SCREEN );
+
+
+    }
+}
