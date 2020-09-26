a02e30a83d6a116ad8e30af760178a28d4cc064d
kartikaysharma01 <sharma.kartik2107@gmail.com>
2020-09-26 21:31:26 +0530

    User Dashboard layout made

--- app/src/main/java/com/example/fuelistic_test/UserDashboard.java
+++ app/src/main/java/com/example/fuelistic_test/UserDashboard.java
@@ -1,32 +1,80 @@
 package com.example.fuelistic_test;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.fuelistic_test.Database.SessionManager;
+import com.google.android.material.navigation.NavigationView;
 
 import java.lang.reflect.TypeVariable;
 import java.util.HashMap;
 
-public class UserDashboard extends AppCompatActivity {
+public class UserDashboard extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    //Variables
+    ImageView homeMenuIcon;
+
+    //Drawer Menu
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_dashboard);
 
-        TextView textView = findViewById(R.id.textView2);
+        //Hooks
+        homeMenuIcon = findViewById(R.id.home_menu_icon);
 
-        SessionManager sessionManager = new SessionManager(this);
-        HashMap<String, String> userDetails = sessionManager.getUserDetailFromSession();
 
-        String fullName = userDetails.get(SessionManager.KEY_FULLNAME);
-        String phooneNo = userDetails.get(SessionManager.KEY_PHONENUMBER);
+        //Menu Hooks
+        drawerLayout = findViewById(R.id.drawer__layout);
+        navigationView = findViewById(R.id.navigation__view);
 
-        textView.setText(fullName+"\n" + phooneNo);
+        navigationDrawer();
+
+
     }
 
+    private void navigationDrawer() {
 
+        //Navigation Drawer
+        navigationView.bringToFront();
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setCheckedItem(R.id.nav__home);
+
+        homeMenuIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (drawerLayout.isDrawerVisible(GravityCompat.START))
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                else
+                    drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if (drawerLayout.isDrawerVisible(GravityCompat.START))
+            drawerLayout.closeDrawer(GravityCompat.START);
+        else
+            super.onBackPressed();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return true;
+    }
 }
